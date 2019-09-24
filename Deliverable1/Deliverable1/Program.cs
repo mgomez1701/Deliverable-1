4ee8d5e11ac4e852f0ab59a28882b9ae013444ad
Mike <michael005@me.com>
2019-09-24 03:48:28 -0400

    I noticed the new deliverable was not uploaded correctly. Tryinto reupload now after checking my Grand Circus feedback on the dashboard

--- Deliverable1/Deliverable1/Program.cs
+++ Deliverable1/Deliverable1/Program.cs
@@ -6,28 +6,43 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Deliverable1
+
 {
     class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            var a = 10;
-            var b = 50;
-            var c = 100;
+            int number;
+            int number2;
+            int number3;
+
+
+            Console.WriteLine("Please pick 3 numbers");
+
+            number = Convert.ToInt32(Console.ReadLine());
+            number2 = Convert.ToInt32(Console.ReadLine());
+            number3 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("{0}, {1}, {2}", number, number2, number3);
+            Console.ReadLine();
 
             String note = "The total of all three numbers is ";
             String message1 = "The average is ";
             String message2 = "The lowest number is ";
             String message3 = "The highest number is ";
 
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-            Console.WriteLine(c);
+            Console.WriteLine(note + ((float)number + (float)number2 + (float)number3));
+            Console.WriteLine(message1 + ((float)number + (float)number2 + (float)number3) / (3));
+            Console.WriteLine(message3);
+            Console.WriteLine(Math.Max(Math.Max(number, number2), number3));
+            Console.WriteLine(message2);
 
-            Console.WriteLine(note + ((float)a + (float)b + (float)c));
-            Console.WriteLine(message1 + ((float)a + (float)b + (float)c) / (3));
-            Console.WriteLine(message2 + ((float)a));
-            Console.WriteLine(message3 + ((float)c));
+            Console.WriteLine(Math.Min(Math.Min(number, number2), number3));
+            Console.ReadLine();
+
+
+            Console.WriteLine("Here are what your numbers look like in 3 different currencies");
 
             CultureInfo ci_US = new CultureInfo("en-US");
             System.Globalization.CultureInfo ci_SE = new CultureInfo("en-SE");
@@ -35,19 +50,19 @@ namespace Deliverable1
             System.Globalization.CultureInfo ci_TH = new CultureInfo("en-TH");
             CultureInfo ci;
 
-
-
             ci = ci_US;
-            Console.WriteLine(string.Format(ci, "{0:C}, {1:C}, {2:C}", 10, 50, 100));
+            Console.WriteLine(string.Format(ci, "{0:C}, {1:C}, {2:C}", number, number2, number3));
 
             ci = ci_SE;
-            Console.WriteLine(string.Format(ci, "{0:C}, {1:C}, {2:C}", 10, 50, 100));
+            Console.WriteLine(string.Format(ci, "{0:C}, {1:C}, {2:C}", number, number2, number3));
 
             ci = ci_JP;
-            Console.WriteLine(string.Format(ci, "{0:C}, {1:C}, {2:C}", 10, 50, 100));
+
+            Console.WriteLine(string.Format(ci, "{0:C}, {1:C}, {2:C}", (decimal)number, (decimal)number2, (decimal)number3));
 
             ci = ci_TH;
-            Console.WriteLine(string.Format(ci, "{0:C}, {1:C}, {2:C}", 10, 50, 100));
+
+            Console.WriteLine(string.Format(ci, "{0:C}, {1:C}, {2:C}", number, number2, number3));
 
 
 
@@ -57,16 +72,8 @@ namespace Deliverable1
 
 
 
+
     }
-
-
-
-
-
-
 }
-
-
-
 
 
